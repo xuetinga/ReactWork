8c95cc1727a1484d6d1a08292dcc8b194a2282d7
xuetinga <1716237703@qq.com>
2020-10-25 10:17:32 +0800

    Signed-off-by: xuetinga <1716237703@qq.com>

---
+++ five/build/precache-manifest.f82f969f1b09e746bed3c15120d8ed23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85426ddda04f728379a1bc3faee16530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f3e5e1357bb359cb0db",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6adf6f1d418e1db0e0c0",
+    "url": "/static/css/main.9f1dd17b.chunk.css"
+  },
+  {
+    "revision": "3f3e5e1357bb359cb0db",
+    "url": "/static/js/2.4ad8532e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4ad8532e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6adf6f1d418e1db0e0c0",
+    "url": "/static/js/main.76faf72c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
