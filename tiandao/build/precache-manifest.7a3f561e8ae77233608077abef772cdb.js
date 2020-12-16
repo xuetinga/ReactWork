a73b81931576d14735fe49f4747828beb6805b87
xuetinga <1716237703@qq.com>
2020-12-16 15:22:00 +0800

    Signed-off-by: xuetinga <1716237703@qq.com>

---
+++ tiandao/build/precache-manifest.7a3f561e8ae77233608077abef772cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d354c6a0c21de3012cd4b20055ee4f83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99b0144483409210ac9c",
+    "url": "./static/css/2.2ab42f45.chunk.css"
+  },
+  {
+    "revision": "03dc7028ca47b7d9396e",
+    "url": "./static/css/main.1560d66b.chunk.css"
+  },
+  {
+    "revision": "99b0144483409210ac9c",
+    "url": "./static/js/2.2b8f0f85.chunk.js"
+  },
+  {
+    "revision": "de570d365995bd49ca8bf4d75020e9e4",
+    "url": "./static/js/2.2b8f0f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dc7028ca47b7d9396e",
+    "url": "./static/js/main.6ab0f0b3.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
